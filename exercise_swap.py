927e73c9385aff4a6a73e26276bf9b2af4b807d9
Joaquin-Silv4 <Jsilva3@mail.austral.edu.ar>
2026-03-13 09:21:40 -0300

    Update exercise_swap.py

--- exercise_swap.py
+++ exercise_swap.py
@@ -10,3 +10,11 @@ def swap():
     """
     x = 10
     y = 20
+    valor_original_x=x
+    valor_original_y=y
+    valor_intercambiado_x=y
+    valor_intercambiado_y=x
+    print(valor_original_x)
+    print(valor_original_y)
+    print(valor_intercambiado_x)
+    print(valor_intercambiado_y)
